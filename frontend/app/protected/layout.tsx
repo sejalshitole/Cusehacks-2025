9425f74de882e140c6d823ca554154305a833a54
Sejal Shitole <sejalshitole@Mac-84.lan>
2025-10-26 03:01:43 -0400

    fix: resolve merge conflict

--- frontend/app/protected/layout.tsx
+++ frontend/app/protected/layout.tsx
@@ -10,24 +10,9 @@ export default function ProtectedLayout({
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <Header />
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+        <div className="flex-1 flex flex-col gap-20  p-5">
           {children}
         </div>
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
       </div>
     </main>
   );
